7c0801bd2142a791032908ff12df25c29e0867d8
Saikrishna Rajaraman <srajara4@ncsu.edu>
2023-10-14 10:13:56 -0400

    Sign-in/Sign-up logic added

--- Slash_Android/app/src/main/java/com/bytes/a/half/slash_android/AuthenticationActivity.kt
+++ Slash_Android/app/src/main/java/com/bytes/a/half/slash_android/AuthenticationActivity.kt
@@ -2,6 +2,7 @@ package com.bytes.a.half.slash_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,14 +21,16 @@ class AuthenticationActivity : ComponentActivity() {
 
     val auth = Firebase.auth
 
+    companion object {
+        const val TAG = "Authentication"     // no i18n
+    }
+
 
     override fun onStart() {
         super.onStart()
         val currentUser = auth.currentUser
         if (currentUser != null) {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+            startMainActivity()
         }
     }
 
@@ -46,5 +49,45 @@ class AuthenticationActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+    fun onSignup(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    startMainActivity()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    showToast(task.exception?.message ?: "")
+                }
+            }
+    }
+
+
+    fun onSignIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    startMainActivity()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    showToast(task.exception?.message ?: "")
+                }
+            }
     }
 }
